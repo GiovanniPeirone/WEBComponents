8ce0a214e4d3fbd8c17494933293cccfc461dcbb
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-05-27 21:23:34 -0300

    a

---
+++ Desplegable1/Src.js
@@ -0,0 +1,22 @@
+const container = document.querySelector("body");
+
+const img = document.createElement("img");
+img.src = "https://lenguajejs.com/assets/logo.svg";
+img.alt = "Logo Javascript";
+
+container.appendChild(img);
+
+
+function Mostrar() 
+{
+    const ContenedorDIV = document.querySelector("#1");
+    const DIV = document.createElement(`
+    <div>
+        
+    </div>
+    `);
+
+
+
+
+}
